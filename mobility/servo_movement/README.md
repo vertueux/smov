4cb684e0b4e0c200a80c26cea40457b2c0914403
Noah VERTUEUX <81981323+vertueux@users.noreply.github.com>
2022-09-10 22:44:16 +0200

    I finally fixed the servo_movement package (after at least 20 days of tinkering).
    I feel a bit stupid because the solution was very simple, I had always used 'ros2 launch' to launch my packages, while in this type of case where we take I/O, we must use 'ros2 run'. It seems obvious when you think about it. This obviousness was gone when I started to almost get it to work using 'launch'. So I added README.md & removed the keyboard_listener package.

---
+++ mobility/servo_movement/README.md
@@ -0,0 +1,4 @@
+To run this package, copy and paste this: 
+```
+ros2 run servo_movement executable
+```
--- mobility/servo_movement/movement/servo_movement.cc
+++ mobility/servo_movement/movement/servo_movement.cc
@@ -1,107 +1,109 @@
 #include <functional>
 #include <memory>
+#include <termios.h>
 
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
 
 #include "i2cpwm_board_msgs/msg/servo_array.hpp"
 #include "i2cpwm_board_msgs/msg/servo.hpp"
 
-using std::placeholders::_1;
+#define CENTER 306
+#define MINIMUM 83
+#define MAXIMUM 520
 
 namespace smov {
 
-constexpr unsigned int str2int(const char* str, int h = 0)
+int getch()
 {
-  return !str[h] ? 5381 : (str2int(str, h+1) * 33) ^ str[h];
+  static struct termios oldt, newt;
+  tcgetattr( STDIN_FILENO, &oldt);           // save old settings
+  newt = oldt; 
+  newt.c_cc[VMIN] = 0; newt.c_cc[VTIME] = 0;
+  tcsetattr( STDIN_FILENO, TCSANOW, &newt);  // apply new settings
+
+  int c = getchar();  // read character (non-blocking)
+
+  tcsetattr( STDIN_FILENO, TCSANOW, &oldt);  // restore old settings
+  return c;
 }
 
 class ServoControl : public rclcpp::Node {
  public:
+  static int number_of_servos;
+  static i2cpwm_board_msgs::msg::ServoArray servo_array;
+
   ServoControl() 
    : Node("servo_control") {
-      RCLCPP_INFO(this->get_logger(), message);
+    publisher = this->create_publisher<i2cpwm_board_msgs::msg::ServoArray>("/servos_absolute", 1);
 
-      publisher = this->create_publisher<i2cpwm_board_msgs::msg::ServoArray>("/servos_absolute", 1);
-      key_subscription = this->create_subscription<std_msgs::msg::String>(
-        "key_event", 10, std::bind(&ServoControl::topic_callback, this, _1));
+    char terminal_reader = getch();
 
-      for (int i = 0; i < number_of_servos; i++) {
-        i2cpwm_board_msgs::msg::Servo temp_servo;
-
-        temp_servo.servo = i;
-        temp_servo.value = 0;
-        servo_array.servos.push_back(temp_servo);
-      }
+    switch(terminal_reader) {
+      case 'q':
+        RCLCPP_INFO(this->get_logger(), "Ending program.");
+        exit(0);
+        break;
+      case 'x':
+        RCLCPP_INFO(this->get_logger(), "Reset all servos to center.");
+        for (size_t i = 0; i < servo_array.servos.size(); i++) {
+          servo_array.servos[i].value = CENTER;
+        }
+        break;
+      case 'r':
+        RCLCPP_INFO(this->get_logger(), "Reset all servos to 0.");
+        for (size_t i = 0; i < servo_array.servos.size(); i++) {
+          servo_array.servos[i].value = 0;
+        }
+        break;
+      case 'i':
+        RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
+        for (size_t i = 0; i < servo_array.servos.size() && terminal_reader != static_cast<int>(i); i++) {
+          RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
+          servo_array.servos[i].value += 1;
+        }
+        break;
+      case 'd':
+        RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
+        for (size_t i = 0; i < servo_array.servos.size() && terminal_reader != static_cast<int>(i); i++) {
+          RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
+          servo_array.servos[i].value -= 1;
+        }
+        break;
+      case 'm':
+        RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
+        for (size_t i = 0; i < servo_array.servos.size() && terminal_reader != static_cast<int>(i); i++) {
+          RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
+          servo_array.servos[i].value = MINIMUM;
+        }
+        break;
+      case 'l':
+        RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
+        for (size_t i = 0; i < servo_array.servos.size() && terminal_reader != static_cast<int>(i); i++) {
+          RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
+          servo_array.servos[i].value = MAXIMUM;
+        }
+        break;
+      case 'c':
+        RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
+        for (size_t i = 0; i < servo_array.servos.size() && terminal_reader != static_cast<int>(i); i++) {
+          RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
+          servo_array.servos[i].value = CENTER;
+        }
+        break;
     }
-
-    void topic_callback(const std_msgs::msg::String& msg) const {
-      switch(str2int(msg.data.c_str())) {
-        case 'q':
-          RCLCPP_INFO(this->get_logger(), "Ending program.");     
-          break;
-        case 'x':
-          RCLCPP_INFO(this->get_logger(), "Reset all servos to center.");
-          for (size_t i = 0; i < servo_array.servos.size(); i++) 
-            servo_array.servos[i].value = center;
-          break;
-        case 'r':
-          RCLCPP_INFO(this->get_logger(), "Reset all servos to 0.");
-          for (size_t i = 0; i < servo_array.servos.size(); i++) 
-            servo_array.servos[i].value = 0;
-          break;
-        case 'i':
-          RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
-
-          for (size_t i = 0; i < servo_array.servos.size(); i++) {
-            RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
-            servo_array.servos[i].value += 1;
-          }
-          break;
-        case 'd':
-          RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
-
-          for (size_t i = 0; i < servo_array.servos.size(); i++) {
-            RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
-            servo_array.servos[i].value -= 1;
-          }
-          break;
-        case 'm':
-          RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
-
-          for (size_t i = 0; i < servo_array.servos.size(); i++) {
-            RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
-            servo_array.servos[i].value = minimum;
-          }
-          break;
-        case 'l':
-          RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
-
-          for (size_t i = 0; i < servo_array.servos.size(); i++) {
-            RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
-            servo_array.servos[i].value = maximum;
-          }
-          break;
-        case 'c':
-          RCLCPP_INFO(this->get_logger(), "Which servo do you want to change the value to? [1, 12]");
-
-          for (size_t i = 0; i < servo_array.servos.size(); i++) {
-            RCLCPP_INFO(this->get_logger(), "You choose servo number:'%d' ", static_cast<int>(i));
-            servo_array.servos[i].value = center;
-          }
-          break;
-      }
   }
 
  private:
-  int center = 306, minimum = 83, maximum = 520, number_of_servos = 12;
-
-  static i2cpwm_board_msgs::msg::ServoArray servo_array;
   i2cpwm_board_msgs::msg::ServoArray servo_array_absolute;
-
   rclcpp::Publisher<i2cpwm_board_msgs::msg::ServoArray>::SharedPtr publisher;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr key_subscription;
-  
+};
+
+int ServoControl::number_of_servos = 12;
+i2cpwm_board_msgs::msg::ServoArray ServoControl::servo_array;
+
+}
+
+int main(int argc, char* argv[]) {
   const char* message = "\nServo Control for 12 Servos.\n"
   "\nEnter one of the following options:\n"
   "-----------------------------------\n\n"
@@ -114,14 +116,20 @@ class ServoControl : public rclcpp::Node {
   "m: Set a Servo to maximum.\n"
   "n: Set a Servo to minimum.\n"
   "c: Set a Servo to center.\n";
-};
+  
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), message);
 
-i2cpwm_board_msgs::msg::ServoArray ServoControl::servo_array;
-
-}
-
-int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<smov::ServoControl>());
+
+  for (int i = 0; i < smov::ServoControl::number_of_servos; i++) {
+    i2cpwm_board_msgs::msg::Servo temp_servo;
+
+    temp_servo.servo = i;
+    temp_servo.value = 0;
+    smov::ServoControl::servo_array.servos.push_back(temp_servo);
+  }
+  while (rclcpp::ok()) {
+    rclcpp::spin_some(std::make_shared<smov::ServoControl>());
+  }
   rclcpp::shutdown();
 }
