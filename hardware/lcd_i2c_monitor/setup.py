45d646dcd9d42622c0e3931bcbf9ab75c07f6370
Noah VERTUEUX <81981323+vertueux@users.noreply.github.com>
2022-09-12 21:21:18 +0200

    Added the package to access the LCD I2C monitor.
    Made for users who want to use it through their robot.

---
+++ hardware/lcd_i2c_monitor/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'lcd_i2c_monitor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        #(os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Virtuous',
+    maintainer_email='virtuous.open.source@gmail.com',
+    description='Driver for the LCD I2C monitor.',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'example = lcd_i2c_monitor.lcd_monitor_example:main'
+        ],
+    },
+)
