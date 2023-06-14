4518e74b4a7a3309e0b383985a57aa9b10976aac
Vertueux <81981323+vertueux@users.noreply.github.com>
2023-06-14 15:35:38 +0200

    Updated the default script that configures the servos.
    It now has a precise center.

--- scripts/servos/config_back_servos.sh
+++ scripts/servos/config_back_servos.sh
@@ -1,3 +1,4 @@
-ros2 service call /config_servos back_board_msgs/srv/ServosConfig "servos: [{servo: 16, center: 333, range: 100, direction: -1},{servo: 15, center: 333, range: 100, direction: -1},{servo: 14, center: 333, range: 100, direction: -1}]"
-ros2 service call /config_servos back_board_msgs/srv/ServosConfig "servos: [{servo: 1, center: 333, range: 100, direction: 1},{servo: 2, center: 333, range: 100, direction: 1},{servo: 3, center: 333, range: 100, direction: 1}]"
+ros2 service call /config_servos back_board_msgs/srv/ServosConfig "servos: [{servo: 3, center: 210, range: 210, direction: -1},{servo: 14, center: 355, range: 190, direction: 1}]"
+ros2 service call /config_servos back_board_msgs/srv/ServosConfig "servos: [{servo: 16, center: 250, range: 320, direction: -1}]"
+ros2 service call /config_servos back_board_msgs/srv/ServosConfig "servos: [{servo: 1, center: 400, range: 250, direction: 1}]"
 echo "All rear servos have been configured."
