ceaf0ff2f23b3393ee1d53ab3b07eb8fae57a9da
r4stl1n <royal.r4stl1n@gmail.com>
2024-02-03 15:10:22 -0800

    Updated build.sh to include dev option

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,14 @@ fi
 
 echo "INFO: ROS_DISTRO found and set to ${ROS_DISTRO}"
 
-# Simple script to be expanded later
-echo "INFO: Building smov project"
-colcon build
+if [ $1 = "dev" ]; then
+  # Build to generate compile_commands.txt for ide usage
+  echo "INFO: Building smov project with development compile_commands.txt"
+  colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
+else
+  # Build without any special flags
+  echo "INFO: Building smov project"
+  colcon build
+fi
+
 echo "INFO: Complete"
