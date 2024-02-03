7cc976f2b77f1bbeb606e91962445c3fa59bad48
r4stl1n <royal.r4stl1n@gmail.com>
2024-02-03 15:45:05 -0800

    Added changes to build.sh to support no parameters and specifying individual packages

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,14 +7,20 @@ fi
 
 echo "INFO: ROS_DISTRO found and set to ${ROS_DISTRO}"
 
-if [ $1 = "dev" ]; then
+if [ $# -eq 0 ]; then
+  # Build without any special flags
+  echo "INFO: Building smov project"
+  colcon build
+  exit
+fi
+
+
+if [ $1 = "devall" ]; then
   # Build to generate compile_commands.txt for ide usage
   echo "INFO: Building smov project with development compile_commands.txt"
   colcon build --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
 else
   # Build without any special flags
-  echo "INFO: Building smov project"
-  colcon build
+  echo "INFO: Building smov package $1"
+  colcon build --packages-select $1
 fi
-
-echo "INFO: Complete"
