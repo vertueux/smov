f69a01fe7033ae5871eb4c96abc5b3846c5c03d7
Vertueux <81981323+vertueux@users.noreply.github.com>
2024-06-02 14:40:34 +0200

    Updated the "Points and Functions" package.

--- samples/points_and_functions/include/points_and_functions.h
+++ samples/points_and_functions/include/points_and_functions.h
@@ -1,18 +1,31 @@
 #ifndef SET_LEGS_DISTANCE_TO_H_
 #define SET_LEGS_DISTANCE_TO_H_
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
 #include <rclcpp/rclcpp.hpp>
 
 #include <smov/executable.h>
 #include <smov/trigonometry.h>
 
-int _argc;
-char **_argv;
+#define KEY_UP 65
+#define KEY_DOWN 66
+#define KEY_RIGHT 67
+#define KEY_LEFT 68
 
 class PointsAndFunctionsState {
  public:
   STATE_CLASS(PointsAndFunctionsState)
+
+  // Useful to read characters without blocking the program.
+  void init_reader(int echo);
+
+  smov::TrigonometryState trig = smov::TrigonometryState(&front_servos, &back_servos, &front_state_publisher, &back_state_publisher, &upper_leg_length, &lower_leg_length, &leg_width, &hip_body_distance);
   double leg_width = 2.5;
+  smov::Vector3 coord;
+  struct termios old_chars, new_chars;
 };
 
 #endif // SET_LEGS_DISTANCE_TO_H_
--- samples/points_and_functions/src/points_and_functions.cc
+++ samples/points_and_functions/src/points_and_functions.cc
@@ -1,31 +1,82 @@
 #include "points_and_functions.h"
+
+void PointsAndFunctionsState::init_reader(int echo) {
+  fcntl(0, F_SETFL, O_NONBLOCK);
+  tcgetattr(0, &old_chars);                 // Grab old terminal i/o settings. 
+  new_chars = old_chars;                    // Make new settings same as old settings. 
+  new_chars.c_lflag &= ~ICANON;             // Disable buffered i/o. 
+  new_chars.c_lflag &= echo ? ECHO : ~ECHO; // Set echo mode. 
+  tcsetattr(0, TCSANOW, &new_chars);        // Use these new terminal i/o settings now.
+}
   
 void PointsAndFunctionsState::on_start() {
+  init_reader(0);
+
   for (int i = 0; i < 6; i++) {
     front_servos.value[i] = 0.0f;
   }
 
   front_state_publisher->publish(front_servos);
 
-  smov::TrigonometryState trig = smov::TrigonometryState(&front_servos, &back_servos, &front_state_publisher, &back_state_publisher, &upper_leg_length, &lower_leg_length, &leg_width, &hip_body_distance);
-
   front_servos.value[1] = 90.0f;
   front_servos.value[3] = 55.0f;
   front_servos.value[5] = 30.0f; // (val - base) / (max - base) = -1
 
-  smov::Vector3 coord(0, 12, 12);
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Setting default position to (6, 12, 12)");
+  coord.x = 6;
+  coord.y = 12;
+  coord.z = 12;
   trig.set_leg_to(1, coord);
-
-  for (int i = 0; i < 6; i++) {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Values: %f", front_servos.value[i]);
-  }
 }
 
 void PointsAndFunctionsState::on_loop() {
+  // This will called every 10ms.
+  int c = getchar();
+  switch (c) {
+    case KEY_UP:
+      coord.x += (1.0/90.0);
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\033[2J\033[;H");
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Coordinates: (%f, %f, %f)", coord.x, coord.y, coord.z);
+      trig.set_leg_to(1, coord);
+      break;
+    case KEY_DOWN:
+      coord.x -= (1.0/90.0);
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\033[2J\033[;H");
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Coordinates: (%f, %f, %f)", coord.x, coord.y, coord.z);
+      trig.set_leg_to(1, coord);
+      break;
+    case KEY_RIGHT:
+      coord.z += (1.0/90.0);
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\033[2J\033[;H");
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Coordinates: (%f, %f, %f)", coord.x, coord.y, coord.z);
+      trig.set_leg_to(1, coord);
+      break;
+    case KEY_LEFT:
+      coord.z -= (1.0/90.0);
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\033[2J\033[;H");
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Coordinates: (%f, %f, %f)", coord.x, coord.y, coord.z);
+      trig.set_leg_to(1, coord);
+      break;
+    case 'e':
+      coord.y += (1.0/90.0);
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\033[2J\033[;H");
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Coordinates: (%f, %f, %f)", coord.x, coord.y, coord.z);
+      trig.set_leg_to(1, coord);
+      break;
+    case 'c':
+      coord.y -= (1.0/90.0);
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "\033[2J\033[;H");
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Coordinates: (%f, %f, %f)", coord.x, coord.y, coord.z);
+      trig.set_leg_to(1, coord);
+      break;
+  }
+  
+  front_state_publisher->publish(front_servos);
 }
 
 void PointsAndFunctionsState::on_quit() {
+  tcsetattr(STDIN_FILENO, TCSANOW, &old_chars);
 }
 
 // This macro creates the node and the main() input, which spins the node.
-DECLARE_STATE_NODE_CLASS("points_and_functions", PointsAndFunctionsState, 500ms)
+DECLARE_STATE_NODE_CLASS("points_and_functions", PointsAndFunctionsState, 10ms)
