e5f3d3389cd38a00583904a6e41a79075065f6de
Vertueux <81981323+vertueux@users.noreply.github.com>
2024-06-04 21:23:43 +0200

    Fixed walking_simulation.cc.

--- samples/walking_simulation/src/walking_simulation.cc
+++ samples/walking_simulation/src/walking_simulation.cc
@@ -40,40 +40,40 @@ void WalkingSimulationState::on_loop() {
   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "coordinates 4: (%f, %f, %f)", coord4.x, coord4.y, coord4.z);
 
   // Creating a function: f(x) = -sqrt(25 - ((coord1.x / 2) - 5)^2) + 20.
-  if (!done_first_step && (coord1.x >= -6 || coord3.x >= -6)) {
+  if (!done_first_step && (coord1.x >= -6 || coord4.x >= -6)) {
     coord1.y = -sqrt(25.0f - pow(((coord1.x / 2) - 2.0f), 2)) + 15.0f; // We have to multiply by -1 to turn upside down the curve to match.
-    coord3.y = -sqrt(25.0f - pow(((coord3.x / 2) - 2.0f), 2)) + 15.0f; // We have to multiply by -1 to turn upside down the curve to match.
+    coord4.y = -sqrt(25.0f - pow(((coord4.x / 2) - 2.0f), 2)) + 15.0f; // We have to multiply by -1 to turn upside down the curve to match.
 
     coord1.x -= 1.0f;
-    coord3.x -= 1.0f;
+    coord4.x -= 1.0f;
     trig.set_leg_to(1, coord1);
-    trig.set_leg_to(3, coord3);
+    trig.set_leg_to(4, coord4);
   }
   else if (!done_first_step) {
     coord1.x = 14.0f;
     coord1.y = 15.0f;
-    coord3.x = 14.0f;
-    coord3.y = 15.0f;
+    coord4.x = 14.0f;
+    coord4.y = 15.0f;
     trig.set_leg_to(1, coord1);
-    trig.set_leg_to(3, coord3);
+    trig.set_leg_to(4, coord4);
     done_first_step = true;
   }
 
-  if (done_first_step && (coord2.x >= -6 || coord4.x >= -6)) {
+  if (done_first_step && (coord2.x >= -6 || coord3.x >= -6)) {
     coord2.y = -sqrt(25.0f - pow(((coord2.x / 2) - 2.0f), 2)) + 15.0f; // We have to multiply by -1 to turn upside down the curve to match.
-    coord4.y = -sqrt(25.0f - pow(((coord4.x / 2) - 2.0f), 2)) + 15.0f; // We have to multiply by -1 to turn upside down the curve to match.
+    coord3.y = -sqrt(25.0f - pow(((coord3.x / 2) - 2.0f), 2)) + 15.0f; // We have to multiply by -1 to turn upside down the curve to match.
 
     coord2.x -= 1.0f;
-    coord4.x -= 1.0f;
+    coord3.x -= 1.0f;
     trig.set_leg_to(2, coord2);
-    trig.set_leg_to(4, coord4);
+    trig.set_leg_to(3, coord3);
   } else if (done_first_step) {
     coord2.x = 14.0f;
     coord2.y = 15.0f;
-    coord4.x = 14.0f;
-    coord4.y = 15.0f;
+    coord3.x = 14.0f;
+    coord3.y = 15.0f;
     trig.set_leg_to(2, coord2);
-    trig.set_leg_to(4, coord4);
+    trig.set_leg_to(3, coord3);
     done_first_step = false;
   }
 }
