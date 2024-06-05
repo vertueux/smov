ff6cf22000e8ccfad25c1b8ca73fbcd66163b68c
Vertueux <81981323+vertueux@users.noreply.github.com>
2024-06-05 15:19:09 +0200

    Updated smov_bringup package.

--- smov_bringup/launch/dual_board.launch.py
+++ smov_bringup/launch/dual_board.launch.py
@@ -1,7 +1,14 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    config = os.path.join(
+      get_package_share_directory('smov_config'),
+      'dual_board.yaml.example'
+    )
     return LaunchDescription([
         Node(
             package='i2c_pwm_board',
@@ -16,12 +23,12 @@ def generate_launch_description():
         Node(
             package='smov_config',
             executable='servos',
-            parameters=["smov_config/config/dual_board.yaml.example"]
+            parameters=[config]
         ),
         Node(
             package='smov_states',
             executable='node',
-            parameters=["smov_config/config/dual_board.yaml.example"],
+            parameters=[config],
             prefix="bash -c 'sleep 2.0; $0 $@'"
         ),
         Node(
--- smov_bringup/launch/single_board.launch.py
+++ smov_bringup/launch/single_board.launch.py
@@ -2,6 +2,10 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    config = os.path.join(
+      get_package_share_directory('smov_config'),
+      'single_board.yaml.example'
+    )
     return LaunchDescription([
         Node(
             package='i2c_pwm_board',
@@ -11,12 +15,12 @@ def generate_launch_description():
         Node(
             package='smov_config',
             executable='servos',
-            parameters=["smov_config/config/single_board.yaml.example"]
+            parameters=[config]
         ),
         Node(
             package='smov_states',
             executable='node',
-            parameters=["smov_config/config/single_board.yaml.example"],
+            parameters=[config],
             prefix="bash -c 'sleep 2.0; $0 $@'"
         ),
         Node(
